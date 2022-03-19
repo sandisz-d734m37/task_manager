6fe507f46852519d885b8ebda961ec07c2ded1b9
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-19 12:09:08 -0600

    Create WelcomeController class

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'welcome#index'
+  
 end
